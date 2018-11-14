b746df2c7e9b3f21a24dc4fa94f45f284a31962a
Lucy Wall <lhwall@gmail.com>
2018-11-14 17:00:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function printBadges(array){
+  for (let i = 0; i < array.length; i ++)
+  console.log("Welcome " + array[i] + "! You are employee #" + (i + 1) +".")
+  return array
+}
+
+function coinFlip(){
+  return Math.random() >= 0.5;
+}
+
+
+
+function tailsNeverFails(){
+  let tailsCount = 0
+  while (coinFlip()){
+    tailsCount++
+  }
+  return "You got " + tailsCount + " tails in a row!"
+}
